4c695fbf5bf9801af60fe8a56928a3581f047983
vamsikiran15 <vamsikiran15@gmail.com>
2024-05-25 14:36:59 +0530

    changes in sevent page

--- baselinesurvey/src/pages/sixthPage.vue
+++ baselinesurvey/src/pages/sixthPage.vue
@@ -3,7 +3,7 @@
         <ion-card-header color="tertiary"><h5>6.If Land Less Labourers , given the details of the Number of days Employment Availed</h5></ion-card-header>
         <ion-card-contents>
             <ion-card-title color="tertiary"><h4>Name of the Scheme/Project</h4></ion-card-title>
-            <ion-card-header color="tertiary">Watershed :</ion-card-header>
+            <ion-card-header color="tertiary">Watershed</ion-card-header>
             <ion-input
             class="ion-margin-top"
             placeholder="Enter days"
