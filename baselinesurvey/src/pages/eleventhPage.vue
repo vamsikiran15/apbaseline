700f759ad45e5cf838294f0a9598414d43616873
vamsikiran15 <vamsikiran15@gmail.com>
2024-06-10 15:12:55 +0530

    completed Eleventh page

---
+++ baselinesurvey/src/pages/eleventhPage.vue
@@ -0,0 +1,97 @@
+<template>
+        <ion-card>
+            <ion-card-header color="tertiary"><h3>11.Family Expenditure</h3></ion-card-header>
+        </ion-card>
+        <ion-card>
+            <ion-card-header color="tertiary"><h6>items</h6></ion-card-header>
+            <ion-card-content>
+                <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Food"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Education"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Medical/Health"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Maintenance of Vehicles"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Maintenance of Household Equipments"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Mobile"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Cloths"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Electricity Bill"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Water Bill"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Social/Religious Functions"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Others"
+            label-placement="floating"
+          ></ion-input>
+          <ion-input
+            class="ion-margin-top"
+            placeholder="Enter Expenditure(Rs) As per Last Year"
+            fill="outline"
+            label="Total"
+            label-placement="floating"
+          ></ion-input>
+            </ion-card-content>
+        </ion-card>
+</template>
+<script>
+export default{}
+</script>
