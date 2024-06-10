ed8472667e43eee7c0128d19e840dfbccbfe3494
vamsikiran15 <vamsikiran15@gmail.com>
2024-06-10 18:45:34 +0530

    completed fifteenth page

---
+++ baselinesurvey/src/pages/fifteenthPage.vue
@@ -0,0 +1,271 @@
+<template>
+<ion-card>
+<ion-card-header color="tertiary"><h3>Agriculture Implements</h3></ion-card-header>
+<ion-card-title class="ion-margin-top" color="tertiary">Implements</ion-card-title>
+<ion-card-content>
+    <ion-card-header class="ion-margin-top" color="tertiary">Tractor</ion-card-header>
+    <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Tractor"
+                      label-placement="floating"
+                      placeholder="Select number of tractor's"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Sprayer-Manual/Power</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Sprayer-Manual/Power"
+                      label-placement="floating"
+                      placeholder="Select number of Sprayer-Manual/Power"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Cultivators/Harrows</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Cultivators/Harrows"
+                      label-placement="floating"
+                      placeholder="Select number of Cultivators/Harrows"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Seed Drill</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Seed Drill"
+                      label-placement="floating"
+                      placeholder="Select number of Seed Drill's"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Tresher</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Tresher"
+                      label-placement="floating"
+                      placeholder="Select number of tresher's"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Ploughs</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Ploughs"
+                      label-placement="floating"
+                      placeholder="Select number of Ploughs"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Sickles</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Sickles"
+                      label-placement="floating"
+                      placeholder="Select number of sickle's"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Crow Bar</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Crow Bar"
+                      label-placement="floating"
+                      placeholder="Select number of crow bar's"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Bullock Cart</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Bullock Cart"
+                      label-placement="floating"
+                      placeholder="Select number of Bullock cart's"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+          <ion-card-header class="ion-margin-top" color="tertiary">Others</ion-card-header>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Others"
+                      label-placement="floating"
+                      placeholder="Select number"
+                      fill="outline"
+                    >
+                      <ion-select-option value="1">1</ion-select-option>
+                      <ion-select-option value="2">2</ion-select-option>
+                      <ion-select-option value="3">3</ion-select-option>
+                      <ion-select-option value="4">4</ion-select-option>
+                      <ion-select-option value="5">5</ion-select-option>
+          </ion-select>
+          <ion-select
+                      class="ion-margin-top"
+                      interface="popover"
+                      label="Owned/Hired"
+                      label-placement="floating"
+                      placeholder="Select owned/hired"
+                      fill="outline"
+                    >
+                      <ion-select-option value="Owned">Owned</ion-select-option>
+                      <ion-select-option value="Hired">Hired</ion-select-option>
+          </ion-select>
+</ion-card-content>
+</ion-card>
+</template>
+<script>
+export default{}
+</script>
