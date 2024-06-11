405cabbfda074be453a22ea9d4c2dcee3f9aed82
vamsikiran15 <vamsikiran15@gmail.com>
2024-06-11 12:33:07 +0530

    twenty6 first page

---
+++ baselinesurvey/src/pages/twentyfirstPage.vue
@@ -0,0 +1,80 @@
+<template><ion-card>
+    <ion-card-header color="tertiary"><h3>21.Participation in Community Programs</h3></ion-card-header>
+    <ion-card-title color="tertiary" class="ion-margin-top">Name of the Community Program</ion-card-title>
+</ion-card>
+<ion-card>
+    <ion-card-header color="tertiary">Awareness Programs</ion-card-header>
+    <ion-card-subtitle color="tertiary" class="ion-margin-top">Increase in Knowledge/Capacity to Execute/Decision Making/Project Management</ion-card-subtitle>
+    <ion-radio-group value="PoP">
+                    <ion-radio
+                      value="Yes"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >Yes</ion-radio
+                    >
+                    <ion-radio
+                      value="No"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >No</ion-radio
+                    >
+                  </ion-radio-group>
+</ion-card>
+<ion-card>
+    <ion-card-header color="tertiary">Trainings</ion-card-header>
+    <ion-card-subtitle color="tertiary" class="ion-margin-top">Increase in Knowledge/Capacity to Execute/Decision Making/Project Management</ion-card-subtitle>
+    <ion-radio-group value="PoP">
+                    <ion-radio
+                      value="Yes"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >Yes</ion-radio
+                    >
+                    <ion-radio
+                      value="No"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >No</ion-radio
+                    >
+                  </ion-radio-group>
+</ion-card>
+<ion-card>
+    <ion-card-header color="tertiary">Exposure Visits</ion-card-header>
+    <ion-card-subtitle color="tertiary" class="ion-margin-top">Increase in Knowledge/Capacity to Execute/Decision Making/Project Management</ion-card-subtitle>
+    <ion-radio-group value="PoP">
+                    <ion-radio
+                      value="Yes"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >Yes</ion-radio
+                    >
+                    <ion-radio
+                      value="No"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >No</ion-radio
+                    >
+                  </ion-radio-group>
+</ion-card>
+<ion-card>
+    <ion-card-header color="tertiary">Inter-State Field Visits</ion-card-header>
+    <ion-card-subtitle color="tertiary" class="ion-margin-top">Increase in Knowledge/Capacity to Execute/Decision Making/Project Management</ion-card-subtitle>
+    <ion-radio-group value="PoP">
+                    <ion-radio
+                      value="Yes"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >Yes</ion-radio
+                    >
+                    <ion-radio
+                      value="No"
+                      label-placement="fixed"
+                      class="ion-padding"
+                      >No</ion-radio
+                    >
+                  </ion-radio-group>
+</ion-card>
+</template>
+<script>
+export default{}
+</script>
